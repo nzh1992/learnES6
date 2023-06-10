4c156dfa8e82d28815f3735536fd75f823b6ff4c
niziheng <nzh199266@163.com>
2023-06-10 18:44:01 +0800

    js简介和手册

---
+++ 01-JavaScript编程语言/02-手册与规范/t.js
@@ -0,0 +1,19 @@
+/*
+* 1.关于规范
+* 1. ECMA-262规范，包含了大部分深入的、详细的、规范化的关于JS的信息。
+* 2. 关于这门语言的细节最权威的信息来源。
+* */
+
+
+/*
+* 2.手册
+* MDN
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+* */
+
+
+/*
+* 3.兼容性表
+* 查看每个功能的支持表
+* https://caniuse.com/
+* */
---
+++ README.md
@@ -0,0 +1,6 @@
+# 学习资料
+学习内容来自于《现代JavaScript教程》
+https://zh.javascript.info/
+
+# 目标
+重新学习一遍JavaScript，以及ES6语法，为入门前端打基础。
