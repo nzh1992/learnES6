f246cbc9a538024a1b28a617752953d7aa7002ce
niziheng <nzh199266@163.com>
2023-06-12 01:00:33 +0800

    值比较

---
+++ 01-JavaScript编程语言/13-值的比较/不同类型间的比较.js
@@ -0,0 +1,17 @@
+/*
+* 不同类型间的比较
+* 先将对比的对象转化为Number类型
+* */
+
+console.log('2' > 1);
+console.log('1' === 1);
+
+
+// 严格相等
+// 用三个等号
+// 两个等号，判断前会先将运算元转化为Number类型
+// 但是，三个等号不会做类型转换
+console.log(0 == false);
+console.log(0 === false);
+
+
